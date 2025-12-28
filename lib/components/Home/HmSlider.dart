6e957452e7e84062f4cd622992720970525e3655
haocz <haocz@jolywood.cn>
2025-12-28 16:14:18 +0800

    [ADD]完成轮播图

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -11,9 +11,12 @@ class HmSlider extends StatefulWidget {
 }
 
 class _HmSliderState extends State<HmSlider> {
+  CarouselSliderController _controller = CarouselSliderController();
+  int _currentIndex = 0;
   Widget _getSlider() {
     final double screenWidth = MediaQuery.of(context).size.width;
     return CarouselSlider(
+      carouselController: _controller,
       items: List.generate(widget.bannerList.length, (index) {
         return Image.network(
           widget.bannerList[index].imgUrl,
@@ -21,12 +24,73 @@ class _HmSliderState extends State<HmSlider> {
           width: screenWidth,
         );
       }),
-      options: CarouselOptions(viewportFraction: 1, autoPlay: true),
+      options: CarouselOptions(
+        viewportFraction: 1,
+        autoPlay: true,
+        height: 300,
+        onPageChanged: (index, reson) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _getSearch() {
+    return Positioned(
+      top: 10,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          alignment: Alignment.centerLeft,
+          height: 50,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(0, 0, 0, 0.4),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: Text("搜索", style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
+  }
+
+  Widget _getDots() {
+    return Positioned(
+      bottom: 10,
+      left: 0,
+      right: 0,
+      child: SizedBox(
+        height: 40,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(widget.bannerList.length, (index) {
+            return GestureDetector(
+              onTap: () {
+                _controller.animateToPage(index);
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                width: _currentIndex == index ? 20 : 10,
+                height: 6,
+                margin: EdgeInsets.symmetric(horizontal: 5),
+                decoration: BoxDecoration(
+                  color: _currentIndex == index ? Colors.white : Colors.grey,
+                ),
+              ),
+            );
+          }),
+        ),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [_getSlider()]);
+    return Stack(children: [_getSlider(), _getSearch(), _getDots()]);
   }
 }
